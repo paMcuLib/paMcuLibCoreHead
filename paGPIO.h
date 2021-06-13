2c619ba7055a7036eca58e8e257a1283c5cb03c1
ActivePeter <1020401660@qq.com>
2021-06-13 16:00:56 +0800

    是

---
+++ paGPIO.h
@@ -0,0 +1,16 @@
+#ifndef __PAGPIO_H__
+#define __PAGPIO_H__
+
+class paGPIO
+{
+private:
+    /* data */
+public:
+    void write(int port, int pin, int state);
+    void write(int gpio, int state);
+    char read(int port, int pin);
+    char read(int gpio);
+};
+extern paGPIO _G_paGPIO;
+
+#endif // __PAGPIO_H__
