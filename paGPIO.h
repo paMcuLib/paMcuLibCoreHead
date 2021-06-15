a4d751ef911d4654feb48b6a337dcf7a912e2a5c
ActivePeter <1020401660@qq.com>
2021-06-15 13:55:45 +0800

    是

--- paGPIO.h
+++ paGPIO.h
@@ -2,18 +2,29 @@
 #define __PAGPIO_H__
 #include "paBase/_ForCore.h"
 //inline 函数所需头文件
+#include "_paGPIO.types.h"
 #include "paCoreSrc/_paGPIO.inline.inc.h"
 class paGPIO
 {
 private:
     /* data */
 public:
-// void write(int port, int pin, int state);
-// void write(int gpio, int state);
-// char read(int port, int pin);
-// char read(int gpio);
-
 //具体inline函数实现
+/**
+ * functions as following need 2 be created in "paCoreSrc/_paGPIO.inline.h"
+ * the detailed gpio type is from platform
+ * 
+ * paErr init(N_paGPIO::Mode gpioMode, gpio_type pin);
+ *
+ * inline void write(gpio_type pin, char status)
+ * {
+ *    gpio_set_level(pin, status);
+ * }
+ * inline char read(gpio_type pin)
+ * {
+ *     return gpio_get_level(pin);
+ * }
+*/
 #include "paCoreSrc/_paGPIO.inline.h"
 };
 extern paGPIO _G_paGPIO;
